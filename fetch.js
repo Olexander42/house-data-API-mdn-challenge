3f93964197762a83dd766156ab2c4b73b30cc560
Olexander <dushko1996@gmail.com>
2025-12-30 15:19:25 +0200

    implement fetch on-site

--- fetch.js
+++
@@ -1,8 +0,0 @@
-export default async function fetchData(url) { 
-  try {
-    const response = await fetch(url);
-    return await response.json();
-  } catch(error) {
-    console.error(error);
-  }
-}
--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-import fetchData from "./fetch.js";
 import { initForm, getSelectedOptions } from "./filters.js";
-import renderHouses from "./render.js";
+import { initHouseCardTemplate, getUniquePropsKeys } from "./template.js";
+import renderHouses from "./handleSearch.js";
 
-
-const form = document.querySelector("form");
-
-let housesData;
 const url = 'https://mdn.github.io/shared-assets/misc/houses.json';
+let housesData;
 
-await fetchData(url)
-  .then((response) => housesData = response)
-  .catch((error) => console.error(error));
+try {
+  const response = await fetch(url);
+  housesData = await response.json();
+} catch(error) {
+  console.error(error);
+}
 
 initForm(housesData);
 
+const propsKeys = getUniquePropsKeys(housesData);
+const templateCard = initHouseCardTemplate(propsKeys);
+
+const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
   renderHouses(event, getSelectedOptions(), housesData)
 }); 
