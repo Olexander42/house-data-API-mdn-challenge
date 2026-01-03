ad25c38aacd177ec8f7ef6de17468aaaface6592
Olexander <dushko1996@gmail.com>
2026-01-03 14:22:18 +0200

    improve isSameLength

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
+export const isSameLength = (obj1, obj2) => Object.keys(obj1).length === Object.keys(obj2).length;
 export const capitalize = (str) => str.replace(str[0], str[0].toUpperCase());
-export const isSameLength = (arr1, arr2) => arr1.length === arr2.length;
 export const normalizeHTML = (html) => html.replace(/\s*(<|>)\s*/g, '$1').trim(); // remove whitespace between nodes
